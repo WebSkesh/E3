f867a0d64b09ac0fd11baedffaf9d63175000c65
WebSkesh <webskesh@gmail.com>
2018-06-06 17:13:28 +0300

    customers in progress

--- database/migrations/2014_05_23_134157_create_customers_table.php
+++ database/migrations/2014_05_23_134157_create_customers_table.php
@@ -16,6 +16,7 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('password');
             $table->integer('number_of_objects')->default(1);
             $table->string('email')->nullable($value = true);
             $table->string('phone')->nullable($value = true);
