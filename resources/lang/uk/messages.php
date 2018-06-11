7849fb8913a6da486fffc5e6fcb7492c3b92e855
WebSkesh <webskesh@gmail.com>
2018-06-11 13:12:23 +0300

    e3

---
+++ resources/lang/uk/messages.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 11.06.2018
+ * Time: 12:53
+ */
+
+
+return [
+    'energybalance' => 'Енергобаланс',
+    'useEnergyWisely' => 'Використовуй енергію розумно!',
+    'dailySystemMonitoring' => 'Система щоденного моніторингу споживання енергоносіїв',
+    'enterData' => 'Ввести дані',
+    'analysis' => 'Аналіз даних',
+    'admin' => 'Адміністрування',
+    'toMain' => 'На головну',
+    'developed' => 'Розробка',
+    'sakalyuk' => 'Сакалюк Д.C. тел.: +380967848377, e-mail: detton1@ukr.net',
+    'chornyi' => 'Чорний П.І. тел.: +380969137678, e-mail: skesh@ukr.net',
+    'allRightsReserved' => 'Всі права захищено',
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Енергобаланс</title>
+    <title>{{ trans('messages.energybalance') }} {{ $customer->name }}</title>
+    <meta name="keywords" content="{{ trans('messages.energybalance') }}, {{ $customer->name }}">
+    <meta name="description" content="{{ trans('messages.dailySystemMonitoring') }} {{ trans('messages.energybalance') }} {{ $customer->name }}">
 </head>
 <body class="welcome_body">
     <div class="container">
@@ -16,44 +18,43 @@
             </div>
 
             <h2>
-                <b>Енергобаланс</b>
+                <b>{{ trans('messages.energybalance') }}</b>
             </h2>
             <h5>
-               <b>Використовуй енергію розумно!</b> <br />
-                Система щоденного моніторингу споживання енергоносіїв {{ $customer->name }}
+               <b>{{ trans('messages.useEnergyWisely') }}</b> <br />
+                {{ trans('messages.dailySystemMonitoring') }} {{ $customer->name }}
             </h5>
 
             <p style="padding-top: 30px;" />
 
             <a class="btn btn-primary" href="{{ route('enterData') }}">
                 <i class="glyphicon glyphicon-save"></i>
-                Ввести дані
+                {{ trans('messages.enterData') }}
             </a>
 
             <a class="btn btn-success" href="{{ route('analysis') }}">
                 <i class="glyphicon glyphicon-eye-open"></i>
-                Аналіз даних
+                {{ trans('messages.analysis') }}
             </a>
 
             <a class="btn btn-warning" href="{{ route('admin') }}">
                 <i class="glyphicon glyphicon glyphicon-cog"></i>
-                Адміністрування
+                {{ trans('messages.admin') }}
             </a>
 
-            <p /><a href="{{ url("/") }}">На головну</a>
+            <p /><a href="{{ url("/") }}">{{ trans('messages.toMain') }}</a>
 
             <p style="padding-top: 20px;" />
 
 
 
             <p />
-            © Розробка: <br/>
-            Сакалюк Д.C. тел.: +380967848377, e-mail: detton1@ukr.net<br/>
-            Чорний П.І. тел.:+380969137678, e-mail: skesh@ukr.net<br/>
-            Всі права захищено<br/>
+            © {{ trans('messages.developed') }}: <br/>
+            {{ trans('messages.sakalyuk') }}<br/>
+            {{ trans('messages.chornyi') }}<br/>
+            {{ trans('messages.allRightsReserved') }}<br/>
 
             <a target="_blank" title="http://energobalans.com/" href="http://energobalans.com/">www.energobalans.com</a>
-
         </div>
     </div>
 </body>
